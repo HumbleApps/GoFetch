691436f9558770f7c9e7b9cc6ffbf943104b31b9
Prabhjot <prabhjot.singh@letstransport.team>
2021-02-23 19:20:06 +0530

    working commit

---
+++ src/constants/miscData.ts
@@ -0,0 +1,19 @@
+export const SOURCE = 'go_fetch_app';
+export const TOKEN_KEY = 'go_fetch_app_token';
+export const TOKEN_SECRET = 'go_fetch_app_secret';
+
+export const LIMIT = 20;
+
+export const ALL = '*';
+
+export const ONE_SECOND_IN_MS = 1000;
+
+export const ONE_MINUTE_IN_MS = 60 * ONE_SECOND_IN_MS;
+
+export const MAX_OTP_TRIES = 5;
+
+export const ONE_DAY_IN_MS = 86400000;
+
+export const FETCH_LIMIT_TRIP_ITEMS = 10;
+
+export const SMS_ORIGIN_ADDRESS = ['JD-LETSTR', 'AX-LETSTR'];
